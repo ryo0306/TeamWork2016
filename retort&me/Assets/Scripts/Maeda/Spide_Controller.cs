cef0ce6829a8ecd454676477499e63bcba6fad06
PS15A17\vantan <kumakoguma5728@gmail.com>
2016-10-24 09:58:24 +0900

    追尾

---
+++ retort&me/Assets/Scripts/Maeda/Spide_Controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spide_Controller : MonoBehaviour {
+
+
+    //追いかけるターゲット
+    public Transform target;
+    // 回転速度
+    public float rotMax;
+    //敵のスピード
+    private float speed = 0.1f;
+
+    Vector3 move = new Vector3(0, 0, 0);
+
+    // Use this for initialization
+    void Start () {
+   
+     //ゲームオブジェクト名で確認している　↓
+            target = GameObject.Find("Player").transform;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // ターゲットの方向を向く
+        Vector3 vec = target.position - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(vec.x, vec.y, 0)), rotMax);
+        // 正面方向に移動
+        transform.Translate(Vector3.forward * speed); 
+    }
+
+
+}
