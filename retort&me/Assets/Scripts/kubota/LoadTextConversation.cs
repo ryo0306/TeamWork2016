b11ffb1ca26ece7b3fe0111356fd9cbf050a0b27
gab0626 <kyhk.0626@outlook.jp>
2016-11-01 13:56:21 +0900

    マップ情報1ステージ

---
+++ retort&me/Assets/Scripts/kubota/LoadTextConversation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadTextConversation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
