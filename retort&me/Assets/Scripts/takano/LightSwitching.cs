1fd5fcff5cff769cd9137471bca776e217d146d9
PS15A12\vantan <takano00514@gmail.com>
2016-11-11 13:29:24 +0900

    噴水タッチからのlightupのギミック作成

---
+++ retort&me/Assets/Scripts/takano/LightSwitching.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitching : MonoBehaviour {
+    
+    public GameObject 仮Enemy = null;
+
+    public bool isLightUp = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "仮Enemy")
+        {
+            Destroy(仮Enemy);
+        }
+    }
+
+    void Switching()
+    {
+        if (isLightUp == false)
+        {
+            
+            GetComponent<Light>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+        }
+
+        if (isLightUp == true)
+        {
+            
+            GetComponent<Light>().enabled = true;
+            GetComponent<Collider>().enabled = true;
+        }
+    }
+ 
+    void Update () {
+        
+        Switching();
+       
+
+    }
+}
