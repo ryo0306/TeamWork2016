886b106e437ef05c7602a3c9a5edf91ea77dcce9
PS15A17\vantan <kumakoguma5728@gmail.com>
2016-11-18 12:31:38 +0900

    クモの巣　クモの出る場所を指定の位置に出す

--- retort&me/Assets/Scripts/Maeda/SpiderNest.cs
+++ retort&me/Assets/Scripts/Maeda/SpiderNest.cs
@@ -14,6 +14,9 @@ public class SpiderNest : MonoBehaviour {
     [SerializeField]
      private bool hit_nest = false;
 
+    [SerializeField]
+    private Vector3 spiderpos;
+
     // Use this for initialization
     void Start () {
 
@@ -29,10 +32,11 @@ public class SpiderNest : MonoBehaviour {
 
         if (collider.gameObject.tag == "Player"){
             if (hit_nest == false) {
-                Instantiate(spider);
+                Instantiate(spider,new Vector3(spiderpos.x, spiderpos.y,spiderpos.z), Quaternion.identity);
                 hit_nest = true;
                 Debug.Log("当たった");
                 Debug.Log("クモでたよ");
+
             }
 
         }
