6320b1e14e40af04bc22e669f81371df0656756f
gab0626 <kyhk.0626@outlook.jp>
2016-12-02 01:29:56 +0900

    ストーリの読み込みTextConversationを作成。 表示方法が未定なため、未作成。

---
+++ retort&me/Assets/Scripts/kubota/LoadConversation.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class LoadConversation : MonoBehaviour {
+
+	private string[] sentences;
+
+	public string[] Sentences
+	{
+		get { return sentences;}
+	}
+
+	private string[] scenarioDictionary = {
+		"Sample1.csv",
+		"Sample2.csv",
+		"Sample3.csv",
+	};
+
+	string[] didCommaSeparationData;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ReadFile (0);
+	}
+
+	void ReadFile(int dictionaryNumber_)
+	{
+		string path = Application.dataPath + "/Scenes/Kubota/CSVStorys/" + scenarioDictionary [dictionaryNumber_];
+		
+		string[] lines = ReadCsvFoundation.ReadCsvData (path);
+
+		didCommaSeparationData = new string[lines.Length];
+
+		char[] commaSplitter = { ',' };
+
+		sentences = new string[lines.Length];
+
+		for (int i = 0; i < lines.Length; i++) {
+
+
+			didCommaSeparationData = ReadCsvFoundation.DataSeparation(lines [i], commaSplitter, 1);
+		
+			sentences[i] = didCommaSeparationData[0];
+
+			Debug.Log (sentences [i]);
+		
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
