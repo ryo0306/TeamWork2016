6c3f11d09e651b270bfe706ad7dcb30df79ff8ce
PS15A12\vantan <takano00514@gmail.com>
2016-12-19 12:02:18 +0900

    看守の作成・・・入った時に、普通の敵を出現させる

---
+++ retort&me/Assets/Scripts/takano/JailerCollision.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JailerCollision : MonoBehaviour {
+
+    public bool appear = false;
+
+
+    [SerializeField]
+    GameObject SponerObject;
+
+    [SerializeField]
+    GameObject SponerEnemyPrefab;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            appear = true;
+            var obj = (GameObject)Instantiate(SponerEnemyPrefab);
+            obj.transform.position = SponerObject.transform.position;
+
+            Debug.Log(appear);
+        }
+
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            appear = false;
+            Debug.Log(appear);
+        }
+    }
+
+
+        // Update is called once per frame
+        void Update () {
+	
+	}
+}
