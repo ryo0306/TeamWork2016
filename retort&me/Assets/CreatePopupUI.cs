83950c5d24218c27a6a00d0266e5a3db3b09a9e5
gab0626 <kyhk.0626@outlook.jp>
2017-01-03 15:27:02 +0900

    UI追加。 prefabなどはresoucesの中にあるkubota->prefabsを参照

---
+++ retort&me/Assets/CreatePopupUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePopupUI : MonoBehaviour {
+
+	[SerializeField]
+	GameObject PopupPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OpenPopup()
+	{
+
+	}
+
+	void ClosePopup()
+	{
+
+	}
+}
